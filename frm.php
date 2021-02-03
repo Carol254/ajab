23dfc8836f7beebd40dba3c334a6fb72336555f4
Carol254 <wanguic49@gmail.com>
2021-02-03 09:39:13 +0300

    quick
    small change

---
+++ frm.php
@@ -0,0 +1,13 @@
+<html>
+<body style="background-color:#ffff;">
+<form action ="welcome.php"
+method ="post">
+Name:<input type ="text" name ="name">
+<br>
+Email:<input type="text" name="email">
+<br>
+<input type ="submit">
+</form>
+</body>
+
+</html>
